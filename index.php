d9012969b76723b6d793a05cae14aad52af69b23
DaReal-81 <darrellwoolfolk@gmail.com>
2022-12-01 21:07:30 -0600

    Hope this works

--- index.php
+++ index.php
@@ -5,3 +5,16 @@ echo "This is just a start";
 echo "Make a new cahnge";
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>No github Tutorial</title>
+</head>
+<body>
+    <p>Making request like all day</p>
+    
+</body>
+</html>
